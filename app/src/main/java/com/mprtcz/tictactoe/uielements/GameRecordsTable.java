dfb717cc741f0e28d9de544e61aad51e3fdf0951
mprtcz <azet89@gmail.com>
2017-03-27 12:11:42 +0200

    Refactoring;

--- app/src/main/java/com/mprtcz/tictactoe/uielements/GameRecordsTable.java
+++ app/src/main/java/com/mprtcz/tictactoe/uielements/GameRecordsTable.java
@@ -15,6 +15,7 @@ import com.mprtcz.tictactoe.R;
 import com.mprtcz.tictactoe.game.model.GameMove;
 import com.mprtcz.tictactoe.game.model.GameRecord;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -29,41 +30,33 @@ public class GameRecordsTable {
     }
 
     public void populateTableRows(TableLayout tableLayout, List<GameRecord> gameRecords) {
-        tableLayout.addView(getTableHeaders(activity));
+        tableLayout.addView(getRecordTableHeaders());
         for (GameRecord gameRecord :
                 gameRecords) {
-            tableLayout.addView(getTableRow(gameRecord));
+            tableLayout.addView(getTableRowOfRecord(gameRecord));
         }
     }
 
-    private TableRow getTableRow(GameRecord gameRecord) {
-        TableRow tableRow = new TableRow(activity);
-        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
-        tableRow.setLayoutParams(lp);
-        tableRow.setPadding(40, 5, 40, 5);
-        TextView firstPlayerTextView = new TextView(activity);
-        firstPlayerTextView.setText(gameRecord.getPlayerOne().getNickname());
-        TextView secondPlayerTextView = new TextView(activity);
-        secondPlayerTextView.setText(gameRecord.getPlayerTwo().getNickname());
-        TextView dateTextView = new TextView(activity);
-        dateTextView.setText(gameRecord.getDateTime().getFormattedDate());
-        Button actionButton = new Button(activity);
-        addGameMovesButtonListener(actionButton, gameRecord);
-        actionButton.setText(R.string.show_moves_button);
-        tableRow.addView(firstPlayerTextView);
-        tableRow.addView(secondPlayerTextView);
-        tableRow.addView(dateTextView);
-        tableRow.addView(actionButton);
+    private TableRow getTableRowOfRecord(GameRecord gameRecord) {
+        TableRow tableRow = getRecordTableRow();
+
+        List<View> views = new LinkedList<>();
+        views.add(getTextView(gameRecord.getPlayerOne().getNickname()));
+        views.add(getTextView(gameRecord.getPlayerTwo().getNickname()));
+        views.add(getTextView(gameRecord.getDateTime().getFormattedDate()));
+        views.add(getButton(activity.getString(R.string.show_moves_button), getGameMovesButtonListener(gameRecord)));
+        addViewsToRow(tableRow, views);
+
         return tableRow;
     }
 
-    private void addGameMovesButtonListener(Button button, final GameRecord gameRecord) {
-        button.setOnClickListener(new View.OnClickListener() {
+    private View.OnClickListener getGameMovesButtonListener(final GameRecord gameRecord) {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(activity);
                 builder.setMessage(composeDialogHeaderMessage(gameRecord))
-                        .setView(GameMovesTable.getScrolledTable(activity, gameRecord))
+                        .setView(getScrolledMovesTable(gameRecord))
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
@@ -72,27 +65,40 @@ public class GameRecordsTable {
                         });
                 builder.create().show();
             }
-        });
+        };
     }
 
-    private TableRow getTableHeaders(Activity activity) {
-        TableRow tableRow = new TableRow(activity);
-        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
-        tableRow.setLayoutParams(lp);
-        tableRow.setPadding(40, 5, 40, 5);
-        TextView firstPlayerTextView = new TextView(activity);
-        firstPlayerTextView.setText(R.string.table_row_first_player_header);
-        TextView secondPlayerTextView = new TextView(activity);
-        secondPlayerTextView.setText(R.string.table_row_second_player_header);
-        TextView dateTextView = new TextView(activity);
-        dateTextView.setText(R.string.table_row_date_header);
-        TextView actionTextView = new TextView(activity);
-        actionTextView.setText(R.string.table_row_action);
-        tableRow.addView(firstPlayerTextView);
-        tableRow.addView(secondPlayerTextView);
-        tableRow.addView(dateTextView);
-        tableRow.addView(actionTextView);
+    private TableRow getRecordTableHeaders() {
+        TableRow tableRow = getRecordTableRow();
+
+        List<View> views = new LinkedList<>();
+        views.add(getTextView(activity.getString(R.string.table_row_first_player_header)));
+        views.add(getTextView(activity.getString(R.string.table_row_second_player_header)));
+        views.add(getTextView(activity.getString(R.string.table_row_date_header)));
+        views.add(getTextView(activity.getString(R.string.table_row_action)));
+        addViewsToRow(tableRow, views);
+
         return tableRow;
+    }
+
+    private void addViewsToRow(TableRow row, List<View> views) {
+        for (View v :
+                views) {
+            row.addView(v);
+        }
+    }
+
+    private Button getButton(String text, View.OnClickListener listener) {
+        Button button = new Button(activity);
+        button.setText(text);
+        button.setOnClickListener(listener);
+        return button;
+    }
+
+    private TextView getTextView(String text) {
+        TextView textView = new TextView(activity);
+        textView.setText(text);
+        return textView;
     }
 
     private static String composeDialogHeaderMessage(GameRecord gameRecord) {
@@ -110,63 +116,71 @@ public class GameRecordsTable {
                 " (X)";
     }
 
-    /**
-     * Created by Azet on 2017-03-27.
-     */
 
-    private static class GameMovesTable {
+    private ScrollView getScrolledMovesTable(GameRecord gameRecord) {
+        ScrollView scrollView = new ScrollView(activity);
+        scrollView.addView(getGameMovesTableLayout(gameRecord));
+        return scrollView;
+    }
 
-        static ScrollView getScrolledTable(Activity activity, GameRecord gameRecord) {
-            ScrollView scrollView = new ScrollView(activity);
-            scrollView.addView(getGameMovesTableLayout(activity, gameRecord));
-            return scrollView;
+    private TableLayout getGameMovesTableLayout(GameRecord gameRecord) {
+        TableLayout tableLayout = getMovesTableLayout();
+        tableLayout.addView(getGameMoveTableHeaders());
+        for (GameMove gameMove : gameRecord.getGameMovesList()) {
+            tableLayout.addView(getGameMoveTableRow(gameMove));
         }
+        return tableLayout;
+    }
 
-        static TableLayout getGameMovesTableLayout(Activity activity, GameRecord gameRecord) {
-            TableLayout tableLayout = new TableLayout(activity);
-            TableLayout.LayoutParams params = new TableLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            tableLayout.setLayoutParams(params);
-            tableLayout.setStretchAllColumns(true);
-            tableLayout.addView(getGameMoveTableHeaders(activity));
-            for (GameMove gameMove : gameRecord.getGameMovesList()) {
-                tableLayout.addView(getGameMoveTableRow(activity, gameMove));
-            }
-            return tableLayout;
-        }
+    private TableLayout getMovesTableLayout() {
+        TableLayout tableLayout = new TableLayout(activity);
+        TableLayout.LayoutParams params = new TableLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        tableLayout.setLayoutParams(params);
+        tableLayout.setStretchAllColumns(true);
+        return tableLayout;
+    }
 
-        private static TableRow getGameMoveTableRow(Activity activity, GameMove gameMove) {
-            TableRow tableRow = new TableRow(activity);
-            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
-            tableRow.setLayoutParams(lp);
-            tableRow.setPadding(40, 5, 40, 5);
-            TextView fieldTextView = new TextView(activity);
-            TextView symbolTextView = new TextView(activity);
-            TextView dateTextView = new TextView(activity);
-            fieldTextView.setText(gameMove.getField());
-            symbolTextView.setText(gameMove.getSymbol());
-            dateTextView.setText(gameMove.getDateTime().getFormattedTime());
-            tableRow.addView(fieldTextView);
-            tableRow.addView(symbolTextView);
-            tableRow.addView(dateTextView);
-            return tableRow;
-        }
+    private TableRow getGameMoveTableRow(GameMove gameMove) {
+        TableRow tableRow = getMoveTableRow();
 
-        private static TableRow getGameMoveTableHeaders(Activity activity) {
-            TableRow tableRow = new TableRow(activity);
-            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
-            tableRow.setLayoutParams(lp);
-            tableRow.setPadding(40, 5, 40, 5);
-            TextView fieldTextView = new TextView(activity);
-            TextView symbolTextView = new TextView(activity);
-            TextView dateTextView = new TextView(activity);
-            fieldTextView.setText(R.string.field_header_game_move);
-            symbolTextView.setText(R.string.symbol_header_game_move);
-            dateTextView.setText(R.string.time_header_game_move);
-            tableRow.addView(fieldTextView);
-            tableRow.addView(symbolTextView);
-            tableRow.addView(dateTextView);
-            return tableRow;
-        }
+        List<View> views = new LinkedList<>();
+        views.add(getTextView(gameMove.getField()));
+        views.add(getTextView(gameMove.getSymbol()));
+        views.add(getTextView(gameMove.getDateTime().getFormattedTime()));
+        addViewsToRow(tableRow, views);
+
+        return tableRow;
+    }
+
+    private TableRow getGameMoveTableHeaders() {
+        TableRow tableRow = getMoveTableRow();
+
+        List<View> views = new LinkedList<>();
+        views.add(getTextView(activity.getString(R.string.field_header_game_move)));
+        views.add(getTextView(activity.getString(R.string.symbol_header_game_move)));
+        views.add(getTextView(activity.getString(R.string.time_header_game_move)));
+        addViewsToRow(tableRow, views);
+
+        return tableRow;
+    }
+
+    private TableRow getRecordTableRow() {
+        TableRow tableRow = getTableRow();
+        tableRow.setPadding(5, 5, 5, 5);
+        return tableRow;
+    }
+
+    private TableRow getTableRow() {
+        TableRow tableRow = new TableRow(activity);
+        TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
+        tableRow.setLayoutParams(lp);
+        return tableRow;
+    }
+
+    private TableRow getMoveTableRow() {
+        TableRow tableRow = getTableRow();
+        tableRow.setPadding(40, 5, 40, 5);
+        return tableRow;
     }
 }
